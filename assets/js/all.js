925dc5ee2784ad3285be7b5a94ea786f4d20b190
elaineliu7598 <girl7598@gmail.com>
2021-08-03 01:04:55 +0800

    Update 2021-08-02T17:04:42.877Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,12 +1,12 @@
+"use strict";
+
 console.log('Hello!');
-
-$(document).ready(() => {
+$(document).ready(function () {
   console.log('HesSchool Hello!');
-
-  $('.dropdown').click(function (e) { 
+  $('.dropdown').click(function (e) {
     e.preventDefault();
     $(this).toggleClass('active');
     $(this).parent().siblings().find('.dropdown').removeClass('active');
   });
-
 });
+//# sourceMappingURL=all.js.map
