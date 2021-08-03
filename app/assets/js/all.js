a62cee3f3d322553516148ee31ad961b7350cf6e
elaineliu7598 <girl7598@gmail.com>
2021-08-04 00:42:38 +0800

    刪除產品頁menu的jQuery

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -3,10 +3,10 @@ console.log('Hello!');
 $(document).ready(() => {
   console.log('HesSchool Hello!');
 
-  $('.dropdown').click(function (e) { 
-    e.preventDefault();
-    $(this).toggleClass('active');
-    $(this).parent().siblings().find('.dropdown').removeClass('active');
-  });
+  // $('.dropdown').click(function (e) { 
+  //   e.preventDefault();
+  //   $(this).toggleClass('active');
+  //   $(this).parent().siblings().find('.dropdown').removeClass('active');
+  // });
 
 });
