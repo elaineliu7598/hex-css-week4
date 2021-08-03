cb89d4c683958dcf1d1577fd09615d62c1c2862b
elaineliu7598 <girl7598@gmail.com>
2021-08-04 00:33:57 +0800

    Update 2021-08-03T16:33:52.954Z

---
+++ README.md
@@ -0,0 +1,10 @@
+### 眼鏡形象網站（PC + RWD）
+
+- [x] 表頭表尾 layout
+- [ ] 首頁 index
+- [x] 系列鏡框 layout-prod、product、product2
+- [x] 門市據點 store、branch
+- [ ] 部落格 blog、blog2
+- [x] 常見問題 faq
+
+
--- assets/js/all.js
+++ assets/js/all.js
@@ -2,11 +2,10 @@
 
 console.log('Hello!');
 $(document).ready(function () {
-  console.log('HesSchool Hello!');
-  $('.dropdown').click(function (e) {
-    e.preventDefault();
-    $(this).toggleClass('active');
-    $(this).parent().siblings().find('.dropdown').removeClass('active');
-  });
+  console.log('HesSchool Hello!'); // $('.dropdown').click(function (e) { 
+  //   e.preventDefault();
+  //   $(this).toggleClass('active');
+  //   $(this).parent().siblings().find('.dropdown').removeClass('active');
+  // });
 });
 //# sourceMappingURL=all.js.map
